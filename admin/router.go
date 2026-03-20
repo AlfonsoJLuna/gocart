7dc0cb871da0a693430a0d573ec04ff97e58c2c1
Alfonso Luna <github@lunaembedded.com>
2026-03-20 10:01:15 +0100

    Added region new and edit pages

--- admin/router.go
+++ admin/router.go
@@ -1,10 +1,11 @@
 package admin
 
 import (
-	"net/http"
 	"html/template"
-	
+	"net/http"
+
 	"go.etcd.io/bbolt"
+
 	"gocart/config"
 )
 
@@ -12,24 +13,31 @@ func Route(cfg *config.Config, db *bbolt.DB, tmpl *template.Template) http.Handl
 	mux := http.NewServeMux()
 
 	// Dashboard
-	mux.HandleFunc("GET /", 						getDashboard(cfg, db, tmpl))
+	mux.HandleFunc("GET /", getDashboard(cfg, db, tmpl))
 
 	// Settings
 
 	// Environment
-    //mux.HandleFunc("GET    /environment",       		getEnvironment(cfg, db, tmpl))
+	//mux.HandleFunc("GET    /environment",       		getEnvironment(cfg, db, tmpl))
 
 	// Currencies
-    mux.HandleFunc("GET		/currencies",       		getCurrencies(cfg, db, tmpl))
-    mux.HandleFunc("GET		/currencies/edit/{id}",  	getCurrenciesEdit(cfg, db, tmpl))
-    mux.HandleFunc("POST	/currencies/edit/{id}",		postCurrenciesEdit(cfg, db, tmpl))
-	mux.HandleFunc("POST	/currencies/reset",			postCurrenciesReset(cfg, db, tmpl))
+	mux.HandleFunc("GET  /currencies", getCurrencies(cfg, db, tmpl))
+	mux.HandleFunc("GET  /currencies/{id}", getCurrenciesEdit(cfg, db, tmpl))
+	mux.HandleFunc("POST /currencies/{id}", postCurrenciesEdit(cfg, db, tmpl))
+	mux.HandleFunc("POST /currencies/reset", postCurrenciesReset(cfg, db, tmpl))
 
 	// Countries
-    mux.HandleFunc("GET		/countries",       			getCountries(cfg, db, tmpl))
-    mux.HandleFunc("GET		/countries/edit/{id}",  	getCountriesEdit(cfg, db, tmpl))
-    mux.HandleFunc("POST	/countries/edit/{id}",		postCountriesEdit(cfg, db, tmpl))
-	mux.HandleFunc("POST	/countries/reset",			postCountriesReset(cfg, db, tmpl))
+	mux.HandleFunc("GET  /countries", getCountries(cfg, db, tmpl))
+	mux.HandleFunc("GET  /countries/{id}", getCountriesEdit(cfg, db, tmpl))
+	mux.HandleFunc("POST /countries/{id}", postCountriesEdit(cfg, db, tmpl))
+	mux.HandleFunc("POST /countries/reset", postCountriesReset(cfg, db, tmpl))
+
+	// Regions
+	mux.HandleFunc("GET  /countries/{id}/region/new", getRegionNew(cfg, db, tmpl))
+	mux.HandleFunc("POST /countries/{id}/region/new", postRegionNew(cfg, db, tmpl))
+	mux.HandleFunc("GET  /countries/{id}/region/{index}", getRegionEdit(cfg, db, tmpl))
+	mux.HandleFunc("POST /countries/{id}/region/{index}", postRegionEdit(cfg, db, tmpl))
+	mux.HandleFunc("POST /countries/{id}/region/{index}/delete", postRegionDelete(cfg, db, tmpl))
 
 	return mux
 }
